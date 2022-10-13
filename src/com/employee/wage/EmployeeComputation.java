fe83628cd3d9f605bcae5fc9ef8518b79810b020
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-13 20:33:00 +0530

    Added: Welcome mesaage.

---
+++ src/com/employee/wage/EmployeeComputation.java
@@ -0,0 +1,12 @@
+package com.employee.wage;
+class WelcomeMessage{
+     static void msg(){
+        System.out.println("Welcome to Employee Wage Computation Program.");
+    }
+}
+public class EmployeeComputation {
+    public static void main(String[] args) {
+
+        WelcomeMessage.msg();
+    }
+}
