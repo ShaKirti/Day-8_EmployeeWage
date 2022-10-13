90f6160e717482abc12bf677ba524b4dede3cd0e
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-14 02:19:49 +0530

    Added: Calculate wages till a condition of total working hours or days is reached for a month

--- src/com/employee/wage/EmployeeComputation.java
+++ src/com/employee/wage/EmployeeComputation.java
@@ -8,9 +8,11 @@ public class EmployeeComputation {
         int workingDayPerMonth = 20;
         int monthlySalary=0;
         int salary=0;
+        int workingHourPerMonth = 0;
+        int days = 1;
         WelcomeMessage.msg();
         WelcomeMessage attendance = new WelcomeMessage();
-        for (int i=0; i<=workingDayPerMonth; i++) {
+        while (days != 20 && workingHourPerMonth != 100) {
             switch (new Random().nextInt(3)) {
                 case 0:
                     System.out.println("Employee is absent.");
@@ -25,6 +27,8 @@ public class EmployeeComputation {
                     break;
             }
             monthlySalary += salary;
+            System.out.println("Day "+days+" : "+salary);
+            days++;
         }
 
         System.out.println("Employee Monthly Salary is: "+monthlySalary);
