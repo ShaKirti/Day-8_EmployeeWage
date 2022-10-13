387b1de066b814dc926a3333929cb3e96b72c6b3
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-13 22:01:02 +0530

    Added: Program to calculate daily employee wage.

--- src/com/employee/wage/EmployeeComputation.java
+++ src/com/employee/wage/EmployeeComputation.java
@@ -2,13 +2,19 @@ package com.employee.wage;
 public class EmployeeComputation {
     public static void main(String[] args) {
 
+        int wagePerHour=20;
+        int fullDayWorkingHour=8;
+        int salary=0;
         WelcomeMessage.msg();
         WelcomeMessage attendance = new WelcomeMessage();
         if (attendance.attendance()==0){
             System.out.println("Employee is absent.");
         }
-        else
+        else {
             System.out.println("Employee is present.");
+            salary = wagePerHour * fullDayWorkingHour;
+        }
+        System.out.println("Employee Salary is: "+salary);
 
     }
 }
--- src/com/employee/wage/WelcomeMessage.java
+++ src/com/employee/wage/WelcomeMessage.java
@@ -3,11 +3,15 @@ package com.employee.wage;
 import java.util.Random;
 
 class WelcomeMessage{
+    int wagePerHour, fullDayWorkingHour;
     static void msg(){
         System.out.println("Welcome to Employee Wage Computation Program.");
     }
     public int attendance(){
         return new Random().nextInt(2);
 
+    }
+    public int salary(){
+        return wagePerHour * fullDayWorkingHour;
     }
 }
