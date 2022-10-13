197d136a49af5444cb3f16cf60d48b64f4d124c7
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-14 02:01:50 +0530

    Added: Program to calculate monthly salary.

--- src/com/employee/wage/EmployeeComputation.java
+++ src/com/employee/wage/EmployeeComputation.java
@@ -5,23 +5,28 @@ import java.util.Random;
 public class EmployeeComputation {
     public static void main(String[] args) {
 
+        int workingDayPerMonth = 20;
+        int monthlySalary=0;
         int salary=0;
         WelcomeMessage.msg();
         WelcomeMessage attendance = new WelcomeMessage();
-        switch (new Random().nextInt(3)){
-            case 0:
-                System.out.println("Employee is absent.");
-                break;
-            case 1:
-                System.out.println("Employee is present half day.");
-                salary=WelcomeMessage.halfSalary();
-                break;
-            case 2:
-                System.out.println("Employee is present full day.");
-                salary=WelcomeMessage.dailySalary();
-                break;
+        for (int i=0; i<=workingDayPerMonth; i++) {
+            switch (new Random().nextInt(3)) {
+                case 0:
+                    System.out.println("Employee is absent.");
+                    break;
+                case 1:
+                    System.out.println("Employee is present half day.");
+                    salary = WelcomeMessage.halfSalary();
+                    break;
+                case 2:
+                    System.out.println("Employee is present full day.");
+                    salary = WelcomeMessage.dailySalary();
+                    break;
+            }
+            monthlySalary += salary;
         }
-        System.out.println("Employee Salary is: "+salary);
 
+        System.out.println("Employee Monthly Salary is: "+monthlySalary);
     }
 }
